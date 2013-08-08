4d962e6f37454aaa9cdf2c31e88f36ed7b2493ff
Nick Veys <nveys@aramisgroup.com>
2013-08-07 22:40:45 -0500

    Speeding up specs

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -4,15 +4,15 @@ describe Castle::Log::Session do
 
   context 'data file long-flight.csv' do
 
-    let(:file) { Castle::Log::File.new(data_file('long-flight.csv')) }
+    before(:all) { @file = Castle::Log::File.new(data_file('long-flight.csv')) }
 
-    subject { file }
+    subject { @file }
 
     it { should have(1).sessions }
 
     context 'session 1' do
 
-      subject { file.sessions[0] }
+      subject { @file.sessions[0] }
 
       its(:duration) { should be_within(0.1).of(569.2) }
 
